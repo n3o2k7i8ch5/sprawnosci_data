a84b7c9df80068f25420c30c7a34ac0e64fcc8b7
daniel <daniel.iwanicki@armlab.ai>
2026-06-20 12:20:31 +0200

    Migrated icons

--- lib/src/all_spraw_book_slugs.dart
+++ lib/src/all_spraw_book_slugs.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:harcapp_core/values/org.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 
 import 'models.dart';
 
